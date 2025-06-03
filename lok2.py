4d356eb35b950f9f23f1b3e5f2094a716b945354
adsadwea <puzzledarku@gmail.com>
2025-06-03 15:23:31 +0700

    Update lok2.py

--- lok2.py
+++ lok2.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://d0a72fc00e1905cbdbf4__cr.tr:16a271cc21b51cef@gw.dataimpulse.com:11204',
-        'https': 'https://d0a72fc00e1905cbdbf4__cr.tr:16a271cc21b51cef@gw.dataimpulse.com:11204',
+        'http': 'http://d0a72fc00e1905cbdbf4__cr.bb:16a271cc21b51cef@gw.dataimpulse.com:11204',
+        'https': 'https://d0a72fc00e1905cbdbf4__cr.bb:16a271cc21b51cef@gw.dataimpulse.com:11204',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
